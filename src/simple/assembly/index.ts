4fd23441b9391110c57ca47e4b42414c3b58337b
eyyubmermer <98224771+eyyubmermer@users.noreply.github.com>
2022-05-04 18:31:56 +0300

    model.ts and index.ts were added.

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,26 +1,68 @@
-import { storage, Context } from "near-sdk-as"
+import {
+  context, // visibility into account, contract and blockchain details
+  ContractPromiseBatch, // make asynchronous calls to other contracts and receive callbacks
+  logging, // append to the execution environment log (appears in JS Developer Console when using near-api-js)
+  storage,
+  u128, // append to the execution environment log (appears in JS Developer Console when using near-api-js)
+} from "near-sdk-as";
 
-// return the string 'hello world'
-export function helloWorld(): string {
-  return 'hello world'
-}
+import { AccountId, asNEAR, ONE_NEAR } from "../../utils";
+import { Student, students } from "./model";
 
-// read the given key from account (contract) storage
-export function read(key: string): string {
-  if (storage.hasKey(key)) {
-    return `✅ Key [ ${key} ] has value [ ${storage.getString(key)!} ]`
-  } else {
-    return `🚫 Key [ ${key} ] not found in storage. ( ${storageReport()} )`
+// Health Check
+export function DeploymentCheck(req: string, rep: string): string {
+  storage.setString(req, rep);
+  let result = storage.getString(req);
+  if (result) {
+    return "Everything is OK!";
   }
+  return "There is something wrong";
 }
 
-// write the given value at the given key to account (contract) storage
-export function write(key: string, value: string): string {
-  storage.set(key, value)
-  return `✅ Data saved. ( ${storageReport()} )`
+// Create and Register a Student
+export function enrollStudent(studentName: string, term: u8): Student {
+  logging.log("Student has enrolled!");
+  return Student.enrollStudent(studentName, term);
 }
 
-// private helper method used by read() and write() above
-function storageReport(): string {
-  return `storage [ ${Context.storageUsage} bytes ]`
+export function setAvarage(id: u32, exams: Array<u8>): Student {
+  logging.log("Student's score has been recorded!");
+  return Student.setAvarage(id, exams);
+}
+
+export function getAllStudent(): Student[] {
+  return Student.getAllStudent();
+}
+
+export function endTerm(): Student[] {
+  return Student.endTerm();
+}
+
+
+
+export function feePayment(id: u32, receiver: AccountId): Student {
+  let student = students.getSome(id);
+  assert(context.attachedDeposit == ONE_NEAR, `Please do not send more or less than your fee! Your fee is ${ONE_NEAR}`)
+  assert(receiver == student.sender, `You must send your payment to ${student.sender}`);
+  assert(context.accountBalance > context.attachedDeposit, "Your balance is not enough!");
+  logging.log(`Payment has been completed! ${context.attachedDeposit.toString()} NEAR sent.`);
+  ContractPromiseBatch.create(receiver).transfer(context.attachedDeposit);
+  return Student.feePayment(student.id);
+}
+
+
+// Take a Look to Archieve
+export function bringStudent(id: u32): Student {
+  return Student.bringStudent(id);
+}
+
+
+export function readStudent(): Student[] {
+  return Student.readAnStudent();
+}
+
+
+export function deleteStudent(id: u32): void {
+  logging.log("Student's account has been deleted!");
+  Student.deleteStudent(id);
 }
---
+++ src/simple/assembly/model.ts
@@ -0,0 +1,104 @@
+import {
+    context, // visibility into account, contract and blockchain details 
+    PersistentUnorderedMap, // data structure that wraps storage
+    math, // utility math function for hashing using SHA and Keccak as well as pseudo-random data
+    logging,
+} from "near-sdk-as";
+
+export const students = new PersistentUnorderedMap<u32, Student>("students");
+@nearBindgen
+export class Student {
+    studentName: string;
+    id: u32;
+    sender: string;
+    enrollmentDate: u64;
+    term: u8;
+    exams: Array<u8> = [];
+    avarage: u64;
+    didPay: bool = false;
+
+
+    constructor(studentName: string, term: u8) {
+        this.id = math.hash32<string>(studentName);
+        this.studentName = studentName;
+        this.sender = context.sender;
+        this.enrollmentDate = context.blockTimestamp;
+        this.term = term;
+    }
+
+
+    static enrollStudent(studentName: string, term: u8): Student {
+        const student = new Student(studentName, term);
+        students.set(student.id, student);
+        return student;
+    }
+
+    static setAvarage(id: u32, exams: Array<u8>): Student {
+        const student = students.getSome(id);
+        assert(student.sender == context.sender);
+        const sum = exams.reduce((a, b) => a + b, 0);
+        const avg = (sum / exams.length) || 0;
+        student.avarage = avg;
+        student.exams = exams;
+        students.set(student.id, student);
+        return student;
+    }
+
+
+    static getAllStudent(): Student[] {
+        return students.values();
+    }
+
+    static endTerm(): Student[] {
+        const arrayStudent: Student[] = new Array<Student>();
+        const arrStudent: Student[] = students.values();
+
+        let length: i32 = students.length;
+        for (let i = 0; i < length; i++) {
+            if (arrStudent[i].didPay == false) {
+                students.delete(arrStudent[i].id);
+                if (arrayStudent.includes(arrStudent[i])) {
+                    arrayStudent.splice(arrayStudent.indexOf(arrStudent[i]), 1);
+                };
+            }
+            if (arrStudent[i].didPay == true) {
+                if (arrStudent[i].avarage >= 50) {
+                    arrStudent[i].term += 1;
+                }
+                arrStudent[i].exams = [];
+                arrStudent[i].avarage = 0;
+                arrStudent[i].didPay = false;
+                students.set(arrStudent[i].id, arrStudent[i]);
+                if (!arrayStudent.includes(arrStudent[i])) {
+                    arrayStudent.push(arrStudent[i])
+                };
+            }
+        }
+        return arrayStudent;
+    }
+
+
+    static bringStudent(id: u32): Student {
+        assert(students.contains(id), "Student could not found!");
+        return students.getSome(id);
+    }
+
+    static feePayment(id: u32): Student {
+        const student = students.getSome(id)
+        student.didPay = true;
+        students.set(student.id, student);
+        return student;
+    }
+
+    static readAnStudent(): Student[] {
+        let start: u32 = 0;
+        logging.log("You can donate to an Advertisingman using 'sendGratitude' function!");
+        return students.values(start, students.length);
+    }
+
+    static deleteStudent(id: u32): void {
+        const student = students.getSome(id);
+        assert(student.sender == context.sender, "Insufficient authorization!");
+        students.delete(id);
+    }
+}
